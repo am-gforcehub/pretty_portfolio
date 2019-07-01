d341353f08a3e31f680b1525def93c0b8d087d6c
am-gforce <47460424+am-gforcehub@users.noreply.github.com>
2019-07-01 17:24:35 -0500

    added news api

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var unirest = require("unirest");
 
 var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3300;
 
+unirest
+  .get(
+    "https://community-hacker-news-v1.p.rapidapi.com/topstories.json?print=pretty"
+  )
+  .header("X-RapidAPI-Host", "community-hacker-news-v1.p.rapidapi.com")
+  .header(
+    "X-RapidAPI-Key",
+    "b6eef57956msha4e94eefd0fe4dep1d72ecjsn6fad9eb00d1e"
+  )
+  .end(function(result) {
+    console.log(result.status, result.headers, result.body);
+  });
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -43,5 +56,14 @@ db.sequelize.sync(syncOptions).then(function() {
     );
   });
 });
+
+unirest
+  .post(
+    "https://community-hacker-news-v1.p.rapidapi.com/item/8863.json?print=pretty"
+  )
+  .header("X-RapidAPI-Key", "6eef57956msha4e94eefd0fe4dep1d72ecjsn6fad9eb00d1e")
+  .end(function(result) {
+    console.log(result.status, result.headers, result.body);
+  });
 
 module.exports = app;
